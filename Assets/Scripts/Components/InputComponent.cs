dae7d40e80ffd248319c1fb82c9a9b8fbd7292fb
Jamie Bertram <jamie@novoideation.ca>
2018-08-18 15:36:34 -0600

    Add input component and system

---
+++ Assets/Scripts/Components/InputComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+
+    public float Horizontal;
+    public float Vertical;
+    public bool IsJump;
+    public bool IsPush;
+    public bool IsPull;
+
+}
