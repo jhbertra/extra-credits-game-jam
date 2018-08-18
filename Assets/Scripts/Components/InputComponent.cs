4f9a3a0ec5f799fd3ab2c9129050acc22c9eb00c
Jamie Bertram <jamie@novoideation.ca>
2018-08-18 15:54:34 -0600

    Implement Input system

--- Assets/Scripts/Components/InputComponent.cs
+++ Assets/Scripts/Components/InputComponent.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class InputComponent : MonoBehaviour
+namespace Scripts.Components
 {
+    public class InputComponent : MonoBehaviour
+    {
 
-    public float Horizontal;
-    public float Vertical;
-    public bool IsJump;
-    public bool IsPush;
-    public bool IsPull;
+        public float Horizontal;
+        public float Vertical;
+        public bool IsJump;
+        public bool IsPush;
+        public bool IsPull;
 
+    }
 }
+
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+namespace Scripts
+{
+    public static class Constants
+    {
+        public const string HorizontalAxis = "Horizontal";
+        public const string VerticalAxis = "Vertical";
+        public const string JumpKey = "Jump";
+        public const string PushKey = "Push";
+        public const string PullKey = "Pull";
+    }
+}
