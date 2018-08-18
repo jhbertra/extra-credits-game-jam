47523d1cfd74fe560410a77aab886d7afe5cadaf
Jamie Bertram <jamie@novoideation.ca>
2018-08-18 16:25:11 -0600

    Add UpdateSpeed

--- Assets/Scripts/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -6,7 +6,7 @@ namespace Scripts.Systems
 {
     public class PlayerMovementSystem : ComponentSystem
     {
-        private struct Components
+        private struct Entity
         {
             public Transform Transform;
             public InputComponent Input;
@@ -15,18 +15,29 @@ namespace Scripts.Systems
 
         protected override void OnUpdate()
         {
-            foreach (var entity in this.GetEntities<Components>())
+            foreach (var entity in this.GetEntities<Entity>())
             {
-                var position = entity.Transform.position;
-                var scale = entity.Transform.localScale;
-
-                position.x += entity.Speed.HorizontalSpeed * Time.deltaTime;
-                scale.x = entity.Speed.HorizontalSpeed > 0
-                    ? 1
-                    : entity.Speed.HorizontalSpeed < 0
-                        ? -1
-                        : scale.x;
+                PlayerMovementSystem.UpdateSpeed(entity);
+                PlayerMovementSystem.UpdatePosition(entity);
             }
+        }
+
+        private static void UpdateSpeed(Entity entity)
+        {
+            entity.Speed.HorizontalSpeed = entity.Input.Horizontal;
+        }
+
+        private static void UpdatePosition(Entity entity)
+        {
+            var position = entity.Transform.position;
+            var scale = entity.Transform.localScale;
+
+            position.x += entity.Speed.HorizontalSpeed * Time.deltaTime;
+            scale.x = entity.Speed.HorizontalSpeed > 0
+                ? 1
+                : entity.Speed.HorizontalSpeed < 0
+                    ? -1
+                    : scale.x;
         }
     }
 }
