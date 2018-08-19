5d1277619fea3428e270df6afc2ef9f8f105f158
Jamie Bertram <jamie@novoideation.ca>
2018-08-18 20:01:19 -0600

    Clean Up Code

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -6,29 +6,29 @@ namespace UnitySampleAssets._2D
     [RequireComponent(typeof (PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
-        private PlatformerCharacter2D character;
-        private bool jump;
-        private bool jumpHold;
-        private bool push;
-        private bool pushHold;
-        private bool pull;
-        private bool pullHold;
+        private PlatformerCharacter2D _character;
+        private bool _jump;
+        private bool _jumpHold;
+        private bool _push;
+        private bool _pushHold;
+        private bool _pull;
+        private bool _pullHold;
 
         private void Awake()
         {
-            character = GetComponent<PlatformerCharacter2D>();
+            this._character = this.GetComponent<PlatformerCharacter2D>();
         }
 
         private void Update()
         {
-            if(!jump)
+            if(!this._jump)
             // Read the jump input in Update so button presses aren't missed.
-            this.jump = Input.GetButtonDown("Jump");
-            this.jumpHold = Input.GetButton("Jump");
-            this.push = Input.GetButtonDown("Push");
-            this.pushHold = Input.GetButton("Push");
-            this.pull = Input.GetButtonDown("Pull");
-            this.pullHold = Input.GetButton("Pull");
+            this._jump = Input.GetButtonDown("Jump");
+            this._jumpHold = Input.GetButton("Jump");
+            this._push = Input.GetButtonDown("Push");
+            this._pushHold = Input.GetButton("Push");
+            this._pull = Input.GetButtonDown("Pull");
+            this._pullHold = Input.GetButton("Pull");
         }
 
         private void FixedUpdate()
@@ -37,8 +37,16 @@ namespace UnitySampleAssets._2D
             bool crouch = Input.GetKey(KeyCode.LeftControl);
             float h = Input.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
-            character.Move(h, crouch, jump, this.jumpHold);
-            jump = false;
+            this._character.Move(
+                h,
+                crouch,
+                this._jump,
+                this._jumpHold,
+                this._push,
+                this._pushHold,
+                this._pull,
+                this._pullHold);
+            this._jump = false;
         }
     }
 }
--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -1,126 +1,128 @@
 ﻿using UnityEngine;
-using UnityEngine.Networking;
+using UnityEngine.Serialization;
 
 namespace UnitySampleAssets._2D
 {
 
     public class PlatformerCharacter2D : MonoBehaviour
     {
-        private bool facingRight = true; // For determining which way the player is currently facing.
+        private bool _facingRight = true; // For determining which way the player is currently facing.
 
-        [SerializeField] private float maxSpeed = 10f; // The fastest the player can travel in the x axis.
-        [SerializeField] private float jumpForce = 400f; // Amount of force added when the player jumps.
-        [SerializeField] private float jumpReleaseDamping = 400f; // Amount of force added when the player jumps.
-        [SerializeField] private float fallMultiplier = 1f; // Amount of force added when the player jumps.
+        [FormerlySerializedAs("maxSpeed")] [SerializeField] private float _maxSpeed = 10f; // The fastest the player can travel in the x axis.
+        [FormerlySerializedAs("jumpForce")] [SerializeField] private float _jumpForce = 400f; // Amount of force added when the player jumps.
+        [FormerlySerializedAs("jumpReleaseDamping")] [SerializeField] private float _jumpReleaseDamping = 400f; // Amount of force added when the player jumps.
+        [FormerlySerializedAs("fallMultiplier")] [SerializeField] private float _fallMultiplier = 1f; // Amount of force added when the player jumps.
 
-        [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;
+        [FormerlySerializedAs("crouchSpeed")] [Range(0, 1)] [SerializeField] private float _crouchSpeed = .36f;
                                                      // Amount of maxSpeed applied to crouching movement. 1 = 100%
 
-        [SerializeField] private bool airControl = false; // Whether or not a player can steer while jumping;
-        [SerializeField] private LayerMask whatIsGround; // A mask determining what is ground to the character
+        [FormerlySerializedAs("airControl")] [SerializeField] private bool _airControl; // Whether or not a player can steer while jumping;
+        [FormerlySerializedAs("whatIsGround")] [SerializeField] private LayerMask _whatIsGround; // A mask determining what is ground to the character
 
-        private Transform groundCheck; // A position marking where to check if the player is grounded.
-        private float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool grounded = false; // Whether or not the player is grounded.
-        private Transform ceilingCheck; // A position marking where to check for ceilings
-        private float ceilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-        private Animator anim; // Reference to the player's animator component.
-        private Rigidbody2D _rigidBody2d;
-        private float baseGravityScale;
+        private const float GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+        private const float CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+        private Transform _groundCheck; // A position marking where to check if the player is grounded.
+        private bool _grounded; // Whether or not the player is grounded.
+        private Transform _ceilingCheck; // A position marking where to check for ceilings
+        private Animator _anim; // Reference to the player's animator component.
+        private Rigidbody2D _rigidBody2D;
+        private float _baseGravityScale;
 
 
         private void Awake()
         {
             // Setting up references.
-            this.groundCheck = this.transform.Find("GroundCheck");
-            this.ceilingCheck = this.transform.Find("CeilingCheck");
-            this.anim = this.GetComponent<Animator>();
-            this._rigidBody2d = this.GetComponent<Rigidbody2D>();
-            this.baseGravityScale = this._rigidBody2d.gravityScale;
+            this._groundCheck = this.transform.Find("GroundCheck");
+            this._ceilingCheck = this.transform.Find("CeilingCheck");
+            this._anim = this.GetComponent<Animator>();
+            this._rigidBody2D = this.GetComponent<Rigidbody2D>();
+            this._baseGravityScale = this._rigidBody2D.gravityScale;
         }
 
 
         private void FixedUpdate()
         {
             // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-            this.grounded = Physics2D.OverlapCircle(this.groundCheck.position, this.groundedRadius, this.whatIsGround);
-            this.anim.SetBool("Ground", this.grounded);
+            this._grounded = Physics2D.OverlapCircle(this._groundCheck.position, PlatformerCharacter2D.GroundedRadius, this._whatIsGround);
+            this._anim.SetBool("Ground", this._grounded);
 
             // Set the vertical animation
-            this.anim.SetFloat("vSpeed", this._rigidBody2d.velocity.y);
+            this._anim.SetFloat("vSpeed", this._rigidBody2D.velocity.y);
         }
 
 
-        public void Move(float move, bool crouch, bool jump, bool jumpHold)
+        public void Move(
+            float move,
+            bool crouch,
+            bool jump,
+            bool jumpHold,
+            bool push,
+            bool pushHold,
+            bool pull,
+            bool pullHold)
         {
 
-
             // If crouching, check to see if the character can stand up
-            if (!crouch && this.anim.GetBool("Crouch"))
+            if (!crouch && this._anim.GetBool("Crouch"))
             {
                 // If the character has a ceiling preventing them from standing up, keep them crouching
-                if (Physics2D.OverlapCircle(this.ceilingCheck.position, this.ceilingRadius, this.whatIsGround))
+                if (Physics2D.OverlapCircle(this._ceilingCheck.position, PlatformerCharacter2D.CeilingRadius, this._whatIsGround))
+                {
                     crouch = true;
+                }
             }
 
             // Set whether or not the character is crouching in the animator
-            this.anim.SetBool("Crouch", crouch);
+            this._anim.SetBool("Crouch", crouch);
 
             //only control the player if grounded or airControl is turned on
-            if (this.grounded || this.airControl)
+            if (this._grounded || this._airControl)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move* this.crouchSpeed : move);
+                move = (crouch ? move* this._crouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
-                this.anim.SetFloat("Speed", Mathf.Abs(move));
+                this._anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                this._rigidBody2d.velocity = new Vector2(move* this.maxSpeed, this._rigidBody2d.velocity.y);
+                this._rigidBody2D.velocity = new Vector2(move * this._maxSpeed, this._rigidBody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !this.facingRight)
+                if (move > 0 && !this._facingRight
+                    || move < 0 && this._facingRight)
                     // ... flip the player.
-                    this.Flip();
-                    // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && this.facingRight)
-                    // ... flip the player.
-                    this.Flip();
+                {
+                    // Switch the way the player is labelled as facing.
+                    this._facingRight = !this._facingRight;
+
+                    // Multiply the player's x local scale by -1.
+                    var theScale = this.transform.localScale;
+                    theScale.x *= -1;
+                    this.transform.localScale = theScale;
+                }
             }
             // If the player should jump...
-            if (this.grounded && jump && this.anim.GetBool("Ground"))
+            if (this._grounded && jump && this._anim.GetBool("Ground"))
             {
                 // Add a vertical force to the player.
-                this.grounded = false;
-                this.anim.SetBool("Ground", false);
-                this._rigidBody2d.AddForce(new Vector2(0f, this.jumpForce));
+                this._grounded = false;
+                this._anim.SetBool("Ground", false);
+                this._rigidBody2D.AddForce(new Vector2(0f, this._jumpForce));
             }
 
-            if (!this.grounded && !jumpHold && this._rigidBody2d.velocity.y > 0)
+            if (!this._grounded && !jumpHold && this._rigidBody2D.velocity.y > 0)
             {
-                this._rigidBody2d.velocity = new Vector2(this._rigidBody2d.velocity.x, this._rigidBody2d.velocity.y / this.jumpReleaseDamping);
+                this._rigidBody2D.velocity = new Vector2(this._rigidBody2D.velocity.x, this._rigidBody2D.velocity.y / this._jumpReleaseDamping);
             }
 
-            if (this._rigidBody2d.velocity.y <= 0f)
+            if (this._rigidBody2D.velocity.y <= 0f)
             {
-                this._rigidBody2d.gravityScale = this.baseGravityScale * this.fallMultiplier;
+                this._rigidBody2D.gravityScale = this._baseGravityScale * this._fallMultiplier;
             }
             else
             {
-                this._rigidBody2d.gravityScale = this.baseGravityScale;
+                this._rigidBody2D.gravityScale = this._baseGravityScale;
             }
-        }
-
-
-        private void Flip()
-        {
-            // Switch the way the player is labelled as facing.
-            this.facingRight = !this.facingRight;
-
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = this.transform.localScale;
-            theScale.x *= -1;
-            this.transform.localScale = theScale;
         }
     }
 }
