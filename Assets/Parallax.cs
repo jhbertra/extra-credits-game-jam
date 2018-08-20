d1fc639b53be437c27133204e706e30d38f41f0a
Jamie Bertram <jamie@novoideation.ca>
2018-08-19 20:13:29 -0600

    Add parallax

---
+++ Assets/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float Strength;
+
+    private Vector3 _lastParentPosition;
+
+    private void Awake()
+    {
+        this._lastParentPosition = this.transform.parent.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var delta = this.transform.parent.position - this._lastParentPosition;
+        this._lastParentPosition = this.transform.parent.position;
+
+        this.transform.localPosition -= delta * this.Strength;
+    }
+}
