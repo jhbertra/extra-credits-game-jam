b90c1bbfacf650bb098b987bd3b99705d473d6a1
Johann Wentzel <wentzel142@gmail.com>
2018-08-19 19:04:28 -0600

    Updated level design, added base moving platform script

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Vector3 OriginalPosition;
+    public float MovementRange;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this.OriginalPosition = this.transform.position;
+	}
+
+    private void Update()
+    {
+        var positionModifier = new Vector3(Mathf.PingPong(Time.time, this.MovementRange), 0, 0);
+        this.transform.position = this.OriginalPosition + positionModifier;
+    }
+
+
+}
